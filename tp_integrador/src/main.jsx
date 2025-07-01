3328e70ac0c7d37307d0f86cce50c5ca3adbeb52
DarienUnU <baldelomardarian@gmail.com>
2025-07-01 00:29:27 -0300

    Instalo dependencias de Bootstrap

--- tp_integrador/src/main.jsx
+++ tp_integrador/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import Store from './store/Store.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
