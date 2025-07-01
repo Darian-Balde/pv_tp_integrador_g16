152efb5d3ea9e048ba8ef69ce9b9710b549ab095
Lara Valdez <42528720@fi.unju.edu.ar>
2025-06-30 21:58:16 -0300

    Rediseñado el footer: agregado contenido informativo (acerca de, contacto, redes (iconos), repositorio) y corregido el estilo para que se alinee correctamente al final de la página

--- tp_integrador/src/components/Footer.jsx
+++ tp_integrador/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 /* Los # Es para agregar informacion, no supe que ocupar ahi*/
 /*Agregue el Repositorio como un acceso rapido */
 const Footer = () => {
@@ -6,38 +7,34 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-content">
                 <div className="footer-section">
-                    <h4>Agregar</h4>
-                    <ul>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                    </ul>
+                    <h4>Acerca De</h4>
+                    <p>
+                        Esta tienda de artículos en general ofrece una amplia
+                        variedad de productos para todas tus necesidades.
+                        ¡Explorá nuestro catálogo y encontrá lo que buscás!
+                    </p>
                 </div>
 
                 <div className="footer-section">
-                    <h4>Agregar</h4>
+                    <h4>Contacto</h4>
                     <ul>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
+                        <li><a href="#">Email: 42164099@fi.unju.edu.ar</a></li>
+                        <li><a href="#">Teléfono: 123-456-7890</a></li>
+                        <li><a href="#">Dirección: Jujuy,Argentina</a></li>
+
                     </ul>
                 </div>
 
-                <div className="footer-section">
-                    <h4>Agregar</h4>
-                    <ul>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                        <li><a href="#">#</a></li>
-                    </ul>
+                <div className="footer-section footer-social">
+                    <h4>Seguinos</h4>
+                    <div className="social-icons">
+                        <a href="#" aria-label="Facebook"><FaFacebookF /></a>
+                        <a href="#" aria-label="Twitter"><FaTwitter /></a>
+                        <a href="#" aria-label="Instagram"><FaInstagram /></a>
+                    </div>
                 </div>
                 <div className="footer-section">
-                    <h4>Git Hub</h4>
+                    <h4>Github</h4>
                     <div className="footer-links">
                         <a href="https://github.com/Darian-Balde/pv_tp_integrador_g16#">Repositorio Git Hub</a> 
                     </div>
